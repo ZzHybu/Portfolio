d01e983e08e7de825e0ebb29c102b7b85465c513
An <trananbrandl@gmail.com>
2026-04-25 23:19:19 +0200

    fix: perfectly configured local and google fonts, fixed router base paths

--- my-portfolio/src/main.tsx
+++ my-portfolio/src/main.tsx
@@ -2,11 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './app/App';
-import './styles/index.css' // (or whatever your css import is)
+
+/* 👇 We are adding the fonts.css import right here! */
+import './styles/fonts.css'
+import './styles/index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    {/* 👇 Add the basename right here! */}
     <BrowserRouter basename={import.meta.env.BASE_URL}>
       <App />
     </BrowserRouter>
